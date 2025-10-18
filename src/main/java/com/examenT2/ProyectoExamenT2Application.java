ad36c120920b075c1341347e156717de0c3234c4
fochis <fochis@email.com>
2025-10-17 19:57:17 -0500

    mi examen T2

---
+++ src/main/java/com/examenT2/ProyectoExamenT2Application.java
@@ -0,0 +1,13 @@
+package com.examenT2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoExamenT2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoExamenT2Application.class, args);
+	}
+
+}
